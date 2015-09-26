be4a631069b3047f39afa5ff5e9cd0426df7fe27
rudy <347212291@qq.com>
2015-09-26 22:59:58 +0800

    add jweixin.js

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,15 @@
+// controllers
 var pages = require('../controllers/pages_controller');
+var wechats = require('../controllers/wechat_shake_controller');
+
+// middlewares
 var http_auth = require('../middlewares/http_auth');
 
 module.exports = function(app){
   // index
   app.get('/', http_auth, pages.home);
+
+  // wechats
+  app.get('/wechats', wechats.index);
 
 }
