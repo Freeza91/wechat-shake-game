14b9cdb72a509623b971407dd99e74d189f88904
rudy <347212291@qq.com>
2015-10-09 16:15:49 +0800

    add redis zset

--- socket/index.js
+++ socket/index.js
@@ -1,24 +1,26 @@
+var redis = require('../lib/redis');
+
 module.exports = function(io){
 
   io.sockets.on('connect', function (socket){
-
-    var i = 0;
+    var result = [];
 
     setInterval(function(){
-      i += 1;
-      console.log("hello client " + i );
-      socket.broadcast.emit("say", { msg: "hello client " + i });
-    }, 2000);
 
-    // socket.on("get_info", function(data){
-    //   console.log(data);
+      // http://stackoverflow.com/questions/24157632/node-redis-get-zrange-withscores
+      redis.zrevrange('hello-set', 0, -1, 'withscores', function (err, reply){
+        result = reply;
+        for(var i=0; i<reply.length; i++){
+          if(i % 2 == 1){
+            console.log(result[i]);
+          }
+        }
+      })
 
-    //   setInterval(function(){
-    //   socket.emit('say', { msg: "hello get info "} );
-    //   }, 4000);
+      randNum = Math.floor((Math.random() * 10));
+      socket.broadcast.emit("say", { msg: "hello client " + result[randNum] });
 
-    // });
-
+    }, 1000);
   });
 
 }
